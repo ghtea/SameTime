fceef86ecd529acb572a66b9c22b918810c2f8e5
jaehyeon(wiz) <eiirwp@gmail.com>
2021-12-22 23:35:12 +0900

    Initial Commit

---
+++ SameTime/SameTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SameTimeApp.swift
+//  SameTime
+//
+//  Created by wiz on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SameTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
